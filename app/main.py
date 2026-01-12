30f1eed6bf720a15d6dfd03b0b1f19800d1efe7e
haku7a <v.a.neyaskin@gmail.com>
2026-01-12 21:54:18 +0300

    feat(db): setup alembic and migrate database init from main.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,7 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
-from app.db.session import engine
-from app.db.base_class import Base
-from app.models import *
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 
 @app.get("/")
