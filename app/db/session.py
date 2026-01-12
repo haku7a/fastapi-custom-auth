e521d93ed3a3a986385ab161362d1aff41fbb17b
haku7a <v.a.neyaskin@gmail.com>
2026-01-12 21:13:02 +0300

    feat(db): setup infrastructure and Role model

---
+++ app/db/session.py
@@ -0,0 +1,8 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from app.core.config import settings
+
+engine = create_async_engine(settings.DATABASE_URL, echo=True)
+
+AsyncSessionFactory = async_sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False, autoflush=False
+)
--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.db.session import engine
+from app.db.base_class import Base
+from app.models import *
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"status": "ok", "message": "System is running"}
